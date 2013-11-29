f39e9a781f15f697efc075104304d718d8d14138
Alex Blewitt <alex.blewitt@gmail.com>
2013-11-29 21:34:03 +0000

    Chapter 4 - Add a bundles:print command

---
+++ com.packtpub.e4.advanced.console/src/com/packtpub/e4/advanced/console/BundlesCommand.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2014, Alex Blewitt, Bandlem Ltd
+ * Copyright (c) 2014, Packt Publishing Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.packtpub.e4.advanced.console;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+public class BundlesCommand {
+	private BundleContext context;
+	public void activate(BundleContext context) {
+		this.context = context;
+	}
+	public void print() {
+		Bundle[] bundles = context.getBundles();
+		for (int i = 0; i < bundles.length; i++) {
+			Bundle bundle = bundles[i];
+			System.out.println(bundle.getBundleId() + " "
+					+ bundle.getSymbolicName());
+		}
+	}
+}
