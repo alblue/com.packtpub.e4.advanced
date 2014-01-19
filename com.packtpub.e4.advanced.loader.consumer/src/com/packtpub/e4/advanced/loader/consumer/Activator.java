e7278e3fb1b6e1a2f7b0319f326a2737f0f8f83b
Alex Blewitt <alex.blewitt@gmail.com>
2014-01-19 16:07:53 +0000

    Chapter 6 - Create a consumer project

---
+++ com.packtpub.e4.advanced.loader.consumer/src/com/packtpub/e4/advanced/loader/consumer/Activator.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2014, Alex Blewitt, Bandlem Ltd
+ * Copyright (c) 2014, Packt Publishing Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.packtpub.e4.advanced.loader.consumer;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+public class Activator implements BundleActivator {
+	@Override
+	public void start(BundleContext context) throws Exception {
+	}
+	@Override
+	public void stop(BundleContext context) throws Exception {
+	}
+}
