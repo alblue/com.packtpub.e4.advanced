8ad9cc1543ab1a0671dd5aab6d90293fb215978a
Alex Blewitt <alex.blewitt@gmail.com>
2014-01-19 15:59:09 +0000

    Chapter 6 - Register an interface for ServiceLoader mediation

---
+++ com.packtpub.e4.advanced.loader.producer/src/com/packtpub/e4/advanced/loader/producer/HelloWorldRunnable.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2014, Alex Blewitt, Bandlem Ltd
+ * Copyright (c) 2014, Packt Publishing Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.packtpub.e4.advanced.loader.producer;
+public class HelloWorldRunnable implements Runnable {
+	@Override
+	public void run() {
+		System.out.println("Hello World");
+	}
+}
