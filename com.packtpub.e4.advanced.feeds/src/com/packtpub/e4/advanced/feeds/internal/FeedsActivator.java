e13b9c22c856029de604a4110caddc48b850a7eb
Alex Blewitt <alex.blewitt@gmail.com>
2014-01-27 22:09:42 +0000

    Chapter 3 - Using service.ranking to order services

--- com.packtpub.e4.advanced.feeds/src/com/packtpub/e4/advanced/feeds/internal/FeedsActivator.java
+++ com.packtpub.e4.advanced.feeds/src/com/packtpub/e4/advanced/feeds/internal/FeedsActivator.java
@@ -8,15 +8,22 @@
  * http://www.eclipse.org/legal/epl-v10.html
  */
 package com.packtpub.e4.advanced.feeds.internal;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import com.packtpub.e4.advanced.feeds.IFeedParser;
 public class FeedsActivator implements BundleActivator {
+	private Dictionary<String,Object> priority(int priority) {
+		Hashtable<String, Object> dict = new Hashtable<String,Object>();
+		dict.put("service.ranking", new Integer(priority));
+		return dict;
+	}
 	@Override
 	public void start(BundleContext context) throws Exception {
-		context.registerService(IFeedParser.class, new RSSFeedParser(), null);
-		context.registerService(IFeedParser.class, new AtomFeedParser(), null);
-		context.registerService(IFeedParser.class, new MockFeedParser(), null);
+		context.registerService(IFeedParser.class, new RSSFeedParser(), priority(1));
+		context.registerService(IFeedParser.class, new MockFeedParser(), priority(-1));
+		context.registerService(IFeedParser.class, new AtomFeedParser(), priority(2));
 	}
 	@Override
 	public void stop(BundleContext context) throws Exception {
