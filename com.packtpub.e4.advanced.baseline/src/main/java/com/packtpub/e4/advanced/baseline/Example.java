37839853cf34df1a6a26afdd9a7d18572e02c452
Alex Blewitt <alex.blewitt@gmail.com>
2014-07-06 21:06:50 +0100

    Chapter 7 - Add baselining example for Maven

---
+++ com.packtpub.e4.advanced.baseline/src/main/java/com/packtpub/e4/advanced/baseline/Example.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2014, Alex Blewitt, Bandlem Ltd
+ * Copyright (c) 2014, Packt Publishing Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.packtpub.e4.advanced.baseline;
+
+public class Example {
+	// After setting version to 1.0.1 uncomment this method
+	// public void newMethod() {
+	// }
+	public void empty() {
+	}
+}
